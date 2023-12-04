e81e51a1e24847fae35e3005134b0456893c310c
muriz-dev <mhmdrizki.dev@gmail.com>
2023-12-04 11:12:58 +0700

    user can login and register

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('login');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'loginError' => 'Username atau password salah',
+        ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     */
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+    public function register()
+    {
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required'],
+            'username' => ['required', 'unique:users,username'],
+            'password' => ['required'],
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        User::create($validatedData);
+
+        return redirect('/login')->with('registerSuccess', 'Berhasil registrasi, silahkan login');
+    }
+}
